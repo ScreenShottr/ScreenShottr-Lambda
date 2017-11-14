7f2c6fe1f0d5c40a12cf2a014ec609925a509c38
Charles <charles@thecjgcjg.com>
2017-11-14 22:14:32 +0000

    Added metadata API. Fixed some things, not sure what though

--- src/getSignedS3URL.js
+++ src/getSignedS3URL.js
@@ -4,19 +4,37 @@ console.log('Loading function')
 const uuidv4 = require('uuid/v4')
 const aws = require('aws-sdk')
 const s3 = new aws.S3()
+const dynamo = new aws.DynamoDB.DocumentClient()
 
 module.exports.getSignedS3URL = (event, context, callback) => {
   const date = new Date
   const key = uuidv4()
-  var params = {
+  const S3params = {
     Bucket: 'screenshottr-service-images-unprocessed-dev',
     Key: key,
     ACL: 'public-read',
     Expires: 250,
     ContentType: 'binary/octet-stream'
   }
+  const dynamoParams = {
+    TableName: 'screenshottr-service-uploads',
+    Item: {
+      image_id: key,
+      image_status: 'not_yet_uploaded',
+      dates: {
+        created: Date.now()
+      },
+      deleted: false
+    }
+  }
 
-  s3.getSignedUrl('putObject', params, function (error, url) {
+  dynamo.put(dynamoParams, function(error, data) {
+    if (error) {
+      console.log(error)
+    }
+  })
+
+  s3.getSignedUrl('putObject', S3params, function (error, url) {
     if (error) {
       const response = {
         statusCode: 500,
--- src/processUpload.js
+++ src/processUpload.js
@@ -3,20 +3,21 @@ const _ = require('lodash')
 const fileType = require('file-type')
 const aws = require('aws-sdk')
 const s3 = new aws.S3()
+const dynamo = new aws.DynamoDB.DocumentClient()
 
 module.exports.processUpload = (event, context, callback) => {
 
   _.map(event.Records, (event) => {
     let filename = ''
     let mimeType = ''
-    const params = {
+    const S3Params = {
       Bucket: event.s3.bucket.name,
       Key: event.s3.object.key
     }
 
     if (event.s3.object.size > 10485760) {
       console.log(`Deleting ${event.s3.object.key}`)
-      s3.deleteObject(params, (err, data) => {
+      s3.deleteObject(S3Params, (err, data) => {
         if (err) {
           console.log(err)
           callback(null, {})
@@ -26,7 +27,7 @@ module.exports.processUpload = (event, context, callback) => {
       })
       callback(null, {})
     } else {
-      s3.getObject(params, (err, data) => {
+      s3.getObject(S3Params, (err, data) => {
         if (err) {
           console.log(err)
           callback(null, {})
@@ -39,7 +40,7 @@ module.exports.processUpload = (event, context, callback) => {
             mimeType = fileMeta.mime
             const putParams = {
               Body: image,
-              Bucket: 'screenshottr-service-images-saved-dev',
+              Bucket: 'screenshottr-service-images-saved',
               Key: filename,
               ContentType: mimeType,
               ACL: 'public-read'
@@ -50,12 +51,32 @@ module.exports.processUpload = (event, context, callback) => {
                 console.log(err)
                 callback(null, {})
               } else {
+                const dynamoParams = {
+                  TableName: 'screenshottr-service-uploads',
+                  Key: {
+                    image_id: event.s3.object.key
+                  },
+                  UpdateExpression: "set dates.uploaded_at = :modifiedTime, image_url = :image_url, fileMeta = :fileMeta, filename = :filename, image_status = :image_status, image_owner = :image_owner",
+                  ExpressionAttributeValues: {
+                    ":modifiedTime": Date.now(),
+                    ":image_url": `https://i.screenshottr.us/${filename}`,
+                    ":fileMeta": fileMeta,
+                    ":filename": filename,
+                    ":image_status": 'image_uploaded',
+                    ":image_owner": 0
+                  }
+                }
+                dynamo.update(dynamoParams, function(error, data) {
+                  if (error) {
+                    console.log(error)
+                  }
+                })
                 console.log(`PUT ${event.s3.object.key}`)
               }
             })
 
           }
-          s3.deleteObject(params, (err, data) => {
+          s3.deleteObject(S3Params, (err, data) => {
             if (err) {
               console.log(err)
               callback(null, {})
