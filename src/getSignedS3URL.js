9f52c2bf471026969222d22309c1558c98ae8f39
Charles <charles@thecjgcjg.com>
2017-11-14 16:06:12 +0000

    Fixed signed stuff

--- src/getSignedS3URL.js
+++ src/getSignedS3URL.js
@@ -6,32 +6,31 @@ const aws = require('aws-sdk')
 const s3 = new aws.S3()
 
 module.exports.getSignedS3URL = (event, context, callback) => {
-  const key = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8)
-    return v.toString(16)
-  })
-
   var params = {
     Bucket: 'screenshottr-service-images-unprocessed-dev',
-    Key: key,
-    AL: 'public-read',
-      Expires: 250
+    Key: uuidv4(),
+    ACL: 'public-read',
+    Expires: 250
   }
-  s3.getSignedUrl('putObject', params, function (err, url) {
+
+  s3.getSignedUrl('putObject', params, function (error, url) {
     if (error) {
       const response = {
         statusCode: 500,
         body: JSON.stringify({
-          message: "Error"
+          message: error
         })
       }
-     callback({}, response)
+     callback(null, response)
     } else {
       console.log("URL", url)
       const response = {
         statusCode: 200,
         body: JSON.stringify({
-          message: url
+          success: true,
+          response: {
+            signedUrl: url
+          }
         })
       }
       callback(null, response)
